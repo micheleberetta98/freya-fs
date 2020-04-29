caffe47b1c6399ea61f9899caaebe9b5b48031ad
Michele Beretta <beretta.m98@gmail.com>
2020-04-29 11:46:15 +0200

    better help text

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ parser = ArgumentParser(
 
 parser.add_argument('mountpoint',
                     metavar='MOUNT',
-                    help='The mount point of this file system'
+                    help='The mount point of this file system.'
                     )
 parser.add_argument('-d', '--data',
-                    help='The folder in which you have your encrypted .enc files',
+                    help='The folder in which you have your encrypted .enc files.',
+                    required=True
                     )
 parser.add_argument('-m', '--metadata',
-                    help='The folder in which you have your .private and .public metadata files - they must have the same name as the associated .enc file',
+                    help='''The folder in which you have your .private and .public metadata files.
+                    The metadata files in this folder must have the same name as the corresponding .enc file and have to be at the top level (no subfolders).                    
+                    If not specified, the --data folder will be used.''',
                     default=None
                     )
 parser.add_argument('-f', '--foreground',
                     action='store_true',
-                    help='Keep this process in the foreground',
+                    help='Flags that keeps this process in the foreground',
                     default=False
                     )
 
