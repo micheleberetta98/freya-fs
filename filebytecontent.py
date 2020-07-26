d5ffe55221116d278dd1b0b27e04a90fa0e8e0da
Michele Beretta <beretta.m98@gmail.com>
2020-07-26 09:14:03 +0200

    added rwlock on filebytecontent

--- filebytecontent.py
+++ filebytecontent.py
@@ -3,30 +3,60 @@ import threading
 class FileByteContent:
     def __init__(self, text):
         self._text = text
-        self._lock = threading.Lock()
+        self._cond = threading.Condition(threading.Lock())
+        self._readers = 0
+
+    def _r_acquire(self):
+        self._cond.acquire()
+        try:
+            self._readers += 1
+        finally:
+            self._cond.release()
+
+    def _r_release(self):
+        self._cond.acquire()
+        try:
+            self._readers -= 1
+            if self._readers == 0:
+                self._cond.notifyAll()
+        finally:
+            self._cond.release()
+
+    def _w_acquire(self):
+        self._cond.acquire()
+        while self._readers > 0:
+            self._cond.wait()
+
+    def _w_release(self):
+        self._cond.release()
 
     def __len__(self):
-        with self._lock:
-            length = len(self._text)
+        self._r_acquire()
+        length = len(self._text)
+        self._r_release()
         return length
 
     def read_all(self):
-        with self._lock:
-            return self._text
+        self._r_acquire()
+        text = self._text
+        self._r_release()
+        return text
 
     def read_bytes(self, offset, length):
-        with self._lock:
-            text = self._text[offset:offset + length]
+        self._r_acquire()
+        text = self._text[offset:offset + length]
+        self._r_release()
         return text
 
     def write_bytes(self, buf, offset):
+        self._w_acquire()
         bytes_written = len(buf)
-        with self._lock:
-            new_text = self._text[:offset] + buf + self._text[offset+bytes_written:]
-            self._text = new_text
-
+        new_text = self._text[:offset] + buf + self._text[offset+bytes_written:]
+        self._text = new_text
+        self._w_release()
         return bytes_written
 
     def truncate(self, length):
-        with self._lock:
-            self._text = self._text[:length]
+        self._w_acquire()
+        self._text = self._text[:length]
+        self._w_release()
