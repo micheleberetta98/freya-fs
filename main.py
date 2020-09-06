113616510535ea8ec8a6729042b6fe2070fdac0d
Michele Beretta <beretta.m98@gmail.com>
2020-09-06 17:37:56 +0200

    fix help message

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ parser.add_argument('mountpoint',
                     help='The mount point of this file system.'
                     )
 parser.add_argument('-d', '--data',
-                    help='The folder in which you have your encrypted .enc files.',
+                    help='The folder in which you have your encrypted files.',
                     required=True
                     )
 parser.add_argument('-m', '--metadata',
                     help='''The folder in which you have your .private and .public metadata files.
-                    The metadata files in this folder must have the same name as the corresponding .enc file and have to be at the top level (no subfolders).                    
+                    The metadata files in this folder must have the same name and path as the corresponding ecnrypted file.                    
                     If not specified, the --data folder will be used.''',
                     default=None
                     )
